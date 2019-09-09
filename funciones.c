20f6dabb22dbbdd1cf238a97dae9d91a201372c0
JDespo <44578440+JDespo@users.noreply.github.com>
2019-09-09 14:11:17 -0300

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -3,28 +3,30 @@
 
 FILE* Open(const char* archivo, const char* modo)
 {
-
-    FILE *pArchivo= NULL;
-    if((pArchivo = fopen(archivo, modo))== NULL)
-        {exit(EXIT_FAILURE);}
+    FILE *pArchivo = NULL;
+    if((pArchivo = fopen(archivo, modo)) == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    
     return pArchivo;
 }
 
 
-STR_ALUMNOS parcear (char  cad[50])
+STR_ALUMNOS parcear (char  linea[])
 {
     STR_ALUMNOS alumno;
-    char *token = strtok(cad, ",");
-    memset(alumno.nombre,'\0',20);
-    strncpy(alumno.nombre, token,20);
+    char *token = strtok(linea, ";");
+    memset(alumno.nombre, '\0', l_name );
+    strncpy(alumno.nombre, token, l_name);
 
-    memset(alumno.legajo,'\0',10);
-    strncpy(alumno.legajo,strtok(NULL, ","),10);
+    memset(alumno.legajo, '\0',10);
+    strncpy(alumno.legajo, strtok(NULL, ";"), l_legajo);
 
-    float nota1 = atof( strtok(NULL, ","));
-    float nota2 = atof( strtok(NULL, ","));
-    float nota3 = atof( strtok(NULL, ","));
+    float nota1 = atof( strtok(NULL, ";"));
+    float nota2 = atof( strtok(NULL, ";"));
+    float nota3 = atof( strtok(NULL, ";"));
     alumno.promedio = (nota1 + nota2 + nota3) / 3;
     
- return alumno;
+    return alumno;
 }
