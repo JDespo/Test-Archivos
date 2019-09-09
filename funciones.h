8cff1be7965621a012dce62b84a8c050c5dfebd5
JDespo <44578440+JDespo@users.noreply.github.com>
2019-09-09 14:07:48 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-typedef struct{
-    char nombre[20];
-    char legajo[10];
-    float  promedio;
-}STR_ALUMNOS;
+#define l_name 20
+#define l_legajo 9
+#define l_linea 50
+#define nota 6
 
 typedef struct funciones
 {
-    char nombre[20];
+    char nombre[l_name +1];
+
 }STR_ARCHIVOS;
+
+
+typedef struct
+{
+    char nombre[l_name +1];
+    char legajo[l_legajo +1];
+    float  promedio;
+
+}STR_ALUMNOS;
 
 FILE* Open(const char* archivo, const char* modo);
 
-STR_ALUMNOS parcear (char cad[50]);
+STR_ALUMNOS parcear (char linea[]);
